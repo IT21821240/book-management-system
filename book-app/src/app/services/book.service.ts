f5ce068e2d56528de5a1be135755af1bac9409f9
Shobi <it21821240@my.sliit.lk>
2026-02-20 00:54:45 +0530

    Add BookService for API calls (CRUD)

---
+++ book-app/src/app/services/book.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Book } from '../models/book';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  private apiUrl = 'http://localhost:5011/api/books'; // your backend port
+
+  constructor(private http: HttpClient) { }
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.apiUrl, book);
+  }
+
+  updateBook(id: number, book: Book) {
+    return this.http.put(`${this.apiUrl}/${id}`, book);
+  }
+
+  deleteBook(id: number) {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
